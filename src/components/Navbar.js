f59fab5d7ff3596dc9eefd83adca621d4318475f
Roshan Bhatia <prakashbhatia1970@gmail.com>
2023-07-06 13:49:34 +0530

    Initial Structure created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export default function Navbar(props){
 
@@ -33,3 +34,5 @@ export default function Navbar(props){
         </>
     )
 }
+
+Navbar.prototypes = {title: PropTypes.string};
